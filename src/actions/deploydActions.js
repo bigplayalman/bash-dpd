a0f06f676785ea10351a1fd96f8d83c8b90a8aaa
almasbarzhaxynov <almas@oomzie.com>
2018-08-12 21:07:54 -0400

    updated stuff

--- src/actions/deploydActions.js
+++ src/actions/deploydActions.js
@@ -1,11 +1,11 @@
 import { USERINFO } from "../constants/ActionTypes";
-import dpd from "dpd-clientlib";
 
 export function login(user) {
   return async dispatch => {};
 }
 
 export function signup(user) {
+
   return async dispatch => {
     return dpd.signups.post(user).then((response) => {
       console.log(response);
